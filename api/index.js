1fc1280a370bf2600e5656ab02990c01968513ed
Gustavo <gustavo.ribeiro.fernandes1@gmail.com>
2022-10-28 18:04:57 -0300

    Including controllers and routes

--- api/index.js
+++ api/index.js
@@ -1,12 +1,10 @@
 const express = require('express');
+const routes = require('./routes')
 
 const app = express();
-
-app.use(express.json());
-
 const port = process.env.PORT || 3000;
 
-app.get('/', (_, res) => res.status(200).send({ mensagem: 'Boas vindas à API, guerreiro!' }));
+routes(app)
 
 app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`));
 
