8d525d65a70b814e3dea18c849dadca78ac7595f
Gustavo <gustavo.ribeiro.fernandes1@gmail.com>
2022-11-03 15:24:27 -0300

    adding validations

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -13,9 +13,26 @@ module.exports = (sequelize, DataTypes) => {
   }
   Pessoas.init(
     {
-      nome: DataTypes.STRING,
+      nome: {
+        type: DataTypes.STRING,
+        validate: {
+          funcaoValidadora: function (dado) {
+            if (dado.length < 3) {
+              throw new Error('o campo nome deve ter mais de 3 caracteres');
+            }
+          },
+        },
+      },
       ativo: DataTypes.BOOLEAN,
-      email: DataTypes.STRING,
+      email: {
+        type: DataTypes.STRING,
+        validate: {
+          isEmail: {
+            args: true,
+            msg: 'dado do tipo e-mail inválido',
+          },
+        },
+      },
       role: DataTypes.STRING,
     },
     {
