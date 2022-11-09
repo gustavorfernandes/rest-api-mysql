7c494a27f1f1f3d98ab28dd1719aac84d7746c4f
Gustavo <gustavo.ribeiro.fernandes1@gmail.com>
2022-11-09 15:03:29 -0300

    addind documentation and swagger

--- api/index.js
+++ api/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const routes = require('./routes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocs = require('./swagger.json');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 routes(app);
 
