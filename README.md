b22540aeb1d24ce66af4ae5a75675cc2f3b38cae
Gustavo <gustavo.ribeiro.fernandes1@gmail.com>
2022-11-09 09:10:37 -0300

    adding readme and refactoring

---
+++ README.md
@@ -0,0 +1,21 @@
+# Rest API MySQL
+
+Rest API application made with Sequelize and MySQL.
+
+## Starting
+
+Endpoints: 
+
+```bash
+...
+```
+
+Documentation:
+
+```bash
+(swagger)
+```
+
+## Other informations
+
+- This project was made with MVC pattern.
--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const routes = require('./routes')
+const routes = require('./routes');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-routes(app)
+routes(app);
 
 app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`));
 
